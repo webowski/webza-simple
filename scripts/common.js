fe9e24a9d8009f180d8d499b358f65bbf4c1cc36
Aleksandr Osokin <sanvovanych@gmail.com>
2020-07-16 10:17:22 +0300

    restore common.js

---
+++ scripts/common.js
@@ -0,0 +1,12 @@
+// App Components
+import './components/Analytics'
+import './components/VectorIcons'
+import './components/Dropdown'
+import './components/FormField'
+import './components/InputMask'
+import './components/Navigation'
+import './components/Popup'
+import './components/ScrollTo'
+import './components/Slider'
+import './components/Tooltip'
+import './components/ThemeSwitch'
