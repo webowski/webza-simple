7bafe6148c2a4a16a7d2b7a508141607c82f6dd6
Aleksandr Osokin <sanvovanych@gmail.com>
2020-09-16 08:19:10 +0300

    Rename output rollup file to common.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,12 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 
-const dist = 'scripts/min'
-
 export default {
 	input: 'scripts/common.js',
 	output: [
 		{
 			name: 'slider',
-			file: `${dist}/bundle.umd.js`,
+			file: `scripts/min/common.js`,
 			format: 'iife',
 			globals: {
 				'swiper': 'Swiper'
