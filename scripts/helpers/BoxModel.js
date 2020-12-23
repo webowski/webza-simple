871e6438a50983f8a363064c10deb6bce93748bc
Aleksandr Osokin <sanvovanych@gmail.com>
2020-12-23 08:24:56 +0300

    isElementReached

--- scripts/helpers/BoxModel.js
+++ scripts/helpers/BoxModel.js
@@ -1,6 +1,20 @@
-// Box Model Helpers
-// =========================
 export const getHeight = (element) => {
 	let height = element.scrollHeight + 'px';
 	return height;
+}
+
+export const isElementReached = (el, offsetY) {
+	var top = el.offsetTop;
+	var height = el.offsetHeight;
+	var bottom = top + height;
+	var offsetY = offsetY || 0;
+
+	while(el.offsetParent) {
+		el = el.offsetParent;
+		top += el.offsetTop;
+	}
+
+	var pointY = window.pageYOffset + offsetY;
+
+	return top < pointY && bottom > pointY;
 }
