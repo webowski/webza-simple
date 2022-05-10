f4e20b63738c9eb6cf96d3d43be7b17ab434596b
Aleksandr Osokin <sanvovanych@gmail.com>
2022-05-10 23:40:42 +0300

    Icon the template component

---
+++ templates/base/helpers/icon.js
@@ -0,0 +1,23 @@
+
+const Handlebars = require('handlebars')
+
+const icon = (name, options) => {
+
+	let attributes = []
+
+	Object.keys(options.hash).forEach(key => {
+		let escapedKey = Handlebars.escapeExpression(key)
+		let escapedValue = Handlebars.escapeExpression(options.hash[key])
+		attributes.unshift(escapedKey + '="' + escapedValue + '"')
+	})
+
+	attributes = attributes.join(' ')
+
+	let output = `<svg ${attributes}>
+		<use href="/images/icons.min.svg#icon-${name}">
+	</svg>`
+
+  return new Handlebars.SafeString(output)
+}
+
+module.exports = icon
