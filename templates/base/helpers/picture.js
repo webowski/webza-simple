3b094279409ccd29f6e78f3799efdb61c547be03
Aleksandr Osokin <sanvovanych@gmail.com>
2022-05-16 09:13:41 +0300

    Refactor the picture helper

--- templates/base/helpers/picture.js
+++ templates/base/helpers/picture.js
@@ -1,6 +1,6 @@
 const Handlebars = require('handlebars')
 
-class Paths {
+class PathsGen {
 	initial
 	srcset
 	srcsetWebp
@@ -8,18 +8,17 @@ class Paths {
 	constructor(src) {
 
 		this.initial = src.replace('@2x', '')
-		this.srcset = this.initial
-
 		let pathWebp = this.initial.replace(/\.jpg|\.png/, '.webp')
-		this.srcsetWebp = pathWebp
-
 		let is2x = src.match(/@2x/)
 
 		if (is2x) {
 			let pathWebp2x = src.replace(/\.jpg|\.png/, '.webp 2x')
 			let path2x = src + ' 2x'
-			this.srcsetWebp = `${pathWebp}, ${pathWebp2x}`
 			this.srcset = `${this.initial}, ${path2x}`
+			this.srcsetWebp = `${pathWebp}, ${pathWebp2x}`
+		} else {
+			this.srcset = this.initial
+			this.srcsetWebp = pathWebp
 		}
 
 	}
@@ -37,8 +36,7 @@ const picture = (options) => {
 	}
 
 	let attributes = makeAttributesString(options.hash)
-
-	let paths = new Paths(src)
+	let paths = new PathsGen(src)
 
 	let output = `<picture ${attributes}>
 		<source srcset="${paths.srcsetWebp}" type="image/webp">
