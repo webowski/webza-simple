079fedb379164d3e1323b18ab01e1db57a4c037a
Aleksandr Osokin <sanvovanych@gmail.com>
2022-05-19 01:44:55 +0300

    handlebars-loader + html plugin work together

--- src/templates/base/helpers/picture.js
+++ src/templates/base/helpers/picture.js
@@ -1,5 +1,17 @@
 const Handlebars = require('handlebars')
 
+// Handlebars.registerHelper('list', (context, options) => {
+//   let output = '<ul>\n'
+
+//   for (let i = 0, j = context.length; i < j; i++) {
+//     output = output + '\t<li>' + options.fn(context[i]).trim() + '</li>\n'
+//   }
+
+//   output += '</ul>\n'
+
+//   return output
+// })
+
 class PathsGen {
 	initial
 	srcset
--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,5 +1,6 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import fs                from 'fs-extra'
 import path, { resolve } from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const __dirname = resolve()
 const mode = process.env.NODE_ENV || 'development'
@@ -13,32 +14,68 @@ export default {
 		// 	import: resolve('./src/scripts/index.js'),
 		// 	filename: './[name].js'
 		// },
-    template: {
-			import: resolve('./src/templates/index.hbs'),
-			filename: '../templates/index.html'
-		},
+    // template: {
+		// 	import: resolve('./src/templates/index.hbs'),
+		// 	filename: './templates/[name].js'
+		// },
 	},
-	output: {
-		path: path.resolve('dist/scripts'),
-	},
+	// output: {
+	// 	path: path.resolve('dist/scripts'),
+	// },
 	module: {
 		rules: [
+
 			// Templates
 			{
 				test: /\.hbs$/,
-				loader: "handlebars-loader"
+				use: [{
+					loader: 'handlebars-loader',
+					options: {
+						helperDirs: [
+							resolve('src/templates/base/helpers'),
+						],
+						partialDirs: [
+							resolve('src/templates/layouts'),
+							// resolve('src/templates/partials'),
+							// resolve('src/templates/components'),
+						],
+						// debug: true,
+					}
+				}]
 			}
+
 		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: 'templates/index.hbs'
+			template: 'templates/index.hbs',
+			// filename: (entryName) => './dist/' + entryName + '.html',
+			filename: 'index.html',
+
+			// template: 'templates/about.hbs',
+			// filename: 'about.html',
+
+			// template: 'templates/manual.hbs',
+			// filename: 'manual.html',
+
+			minify: false,
+			inject: false,
+			templateParameters: JSON.parse(
+				fs.readFileSync(resolve('src/templates/base/data.json'))
+			)
 		})
 	],
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias: {
+			handlebars: 'handlebars/dist/handlebars.js',
+			// 'express-handlebars': 'handlebars/dist/handlebars.min.js'
+		}
 	},
 	target: target,
+	stats: {
+		// children: true
+	},
 	watchOptions: {
 		ignored: '**/node_modules',
 	},
