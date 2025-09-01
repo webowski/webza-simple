04bfc45432de0ac49354395a9e8fec6ac6a975b0
webowski <okwebowski@gmail.com>
2025-09-02 01:59:13 +0300

    remove unnecessary files

--- template-parts/content-search.php
+++
@@ -1,9 +0,0 @@
-<article>
-	<header class="">
-		<h2><a href="<?php esc_url( get_permalink() ) ?>" rel="bookmark"><?php the_title() ?></a></h2>
-	</header>
-
-	<div class="mt-8">
-		<?php the_excerpt(); ?>
-	</div>
-</article>
